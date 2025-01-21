bcfa3eba12ab53eafe45b402f47aa16a24975de7
Youcef <yousseffboudjadja@gmail.com>
2025-01-21 16:26:33 -0500

    Migration vers tailwind

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,7 @@
-import Header from '@/components/Header'
+import Header from "@/components/Header";
 import { Inter } from "next/font/google";
-import Footer from '@/components/Footer';
+import Footer from "@/components/Footer";
 import "./globals.css";
-import styles from './layout.module.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className +' '+styles.body}>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Header />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,9 @@
-import styles from "./page.module.css";
 import Accueil from "@/components/Accueil";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Accueil/>
+    <main className="flex-1">
+      <Accueil />
     </main>
   );
 }
